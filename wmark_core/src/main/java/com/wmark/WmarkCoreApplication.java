6c43977a4550a38e6cbcabfcb9bab5343c38bcc7
wangshuai <wangshuai@hyjk.tech>
2018-10-29 18:37:00 +0800

    wmark-core create

---
+++ wmark_core/src/main/java/com/wmark/WmarkCoreApplication.java
@@ -0,0 +1,12 @@
+package com.wmark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmarkCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WmarkCoreApplication.class, args);
+    }
+}
